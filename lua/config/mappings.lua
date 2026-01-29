86012441aa081bde6a7fecd959cc3a404c8fbb4f
lominoss-git <79030093+lominoss-git@users.noreply.github.com>
2026-01-29 20:14:50 +0100

    Delete lua/config directory

--- lua/config/mappings.lua
+++
@@ -1,6 +0,0 @@
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
-map("n", "<leader>w", ":w<CR>", opts)
-map("n", "<leader>q", ":q<CR>", opts)
-map("n", "<leader>x", ":x<CR>", opts)
